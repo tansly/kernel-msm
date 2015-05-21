3c0cd18db457285b586339bd69158b824317c3a7
Subbaraya Sundeep Bhatta <subbaraya.sundeep.bhatta@xilinx.com>
2015-05-21 15:46:48 +0530

    usb: dwc3: gadget: return error if command sent to DEPCMD register fails
    commit 76e838c9f7765f9a6205b4d558d75a66104bc60d upstream.
    
    We need to return error to caller if command is not sent to
    controller succesfully.
    
    Signed-off-by: Subbaraya Sundeep Bhatta <sbhatta@xilinx.com>
    Fixes: 72246da40f37 (usb: Introduce DesignWare USB3 DRD Driver)
    Signed-off-by: Felipe Balbi <balbi@ti.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
     Conflicts:
    	drivers/usb/dwc3/gadget.c

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -433,7 +433,9 @@ int dwc3_send_gadget_ep_cmd(struct dwc3 *dwc, unsigned ep,
 			 */
 			if (reg & 0x2000)
 				ret = -EAGAIN;
-			else
+			else if (DWC3_DEPCMD_STATUS(reg))
+				ret = -EINVAL;
+                        else
 				ret = 0;
 			break;
 		}
